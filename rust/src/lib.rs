33d58e95a3073aabe6a692a03ef9edaab1a27594
Savanni D'Gerinel <savanni@luminescent-dreams.com>
2018-07-08 11:43:19 -0400

    Documentation!

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,25 @@
+//! The Orizentic token management library
+//!
+//! This library provides a high level interface for authentication token management.  It wraps
+//! around the [JWT](https://jwt.io/) standard using the
+//! [`jsonwebtoken`](https://github.com/Keats/jsonwebtoken) library for serialization and
+//! validation. 
+//!
+//! Functionality revolves around the relationship between a [ClaimSet](struct.ClaimSet.html), a
+//! [VerifiedToken](struct.VerifiedToken.html), and an
+//! [UnverifiedToken](struct.UnverifiedToken.html). A [ClaimSet](struct.ClaimSet.html) is
+//! considered informative and stores all of the information about the permissions and resources
+//! that the token bearer should have access to.  [VerifiedToken](struct.VerifiedToken.html) and
+//! [UnverifiedToken](struct.UnverifiedToken.html) are the result of the process of decoding a
+//! string JWT, and inherently specify whether the decoding process verified the signature,
+//! expiration time, and presence in the database.
+//!
+//! This library does not currently contain database save and load features, but those are a likely
+//! upcoming feature.
+//!
+//! No setup is necessary when using this library to decode JWT strings. Refer to the standalone
+//! [decode_text](fn.decode_text.html) function.
+
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
@@ -10,29 +32,44 @@ use chrono::prelude::*;
 use uuid::Uuid;
 use std::collections::HashMap;
 
+/// Orizentic Errors
 pub enum Error {
+    /// An underlying JWT decoding error. May be replaced with Orizentic semantic errors to better
+    /// encapsulate the JWT library.
     JWTError(jwt::errors::Error),
+    /// Token decoded and verified but was not present in the database.
     UnknownToken(),
 }
 
+/// ResourceName is application-defined and names a resource to which access should be controlled
 #[derive(Debug, PartialEq, Clone)]
 pub struct ResourceName(pub String);
 
+/// Permissions are application-defined descriptions of what can be done with the named resource
 #[derive(Debug, PartialEq, Clone)]
 pub struct Permissions(pub Vec<String>);
 
+/// Issuers are typically informative, but should generally describe who or what created the token
 #[derive(Debug, PartialEq, Clone)]
 pub struct Issuer(pub String);
 
+/// Time to live is the number of seconds until a token expires. This is used for creating tokens
+/// but tokens store their actual expiration time.
 #[derive(Debug, PartialEq, Clone)]
 pub struct TTL(pub chrono::Duration);
 
+/// Username, or Audience in JWT terms, should describe who or what is supposed to be using this
+/// token
 #[derive(Debug, PartialEq, Clone)]
 pub struct Username(pub String);
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Secret(pub Vec<u8>);
 
+/// A ClaimSet represents one set of permissions and claims. It is a standardized way of specifying
+/// the owner, issuer, expiration time, relevant resources, and specific permissions on that
+/// resource. By itself, this is only an informative data structure and so should never be trusted
+/// when passed over the wire. See `VerifiedToken` and `UnverifiedToken`.
 #[derive(Debug, PartialEq, Clone)]
 pub struct ClaimSet {
     pub id: String,
@@ -44,8 +81,10 @@ pub struct ClaimSet {
     pub permissions: Permissions,
 }
 
+/// ClaimSetJS is an intermediary data structure between JWT serialization and a more usable
+/// ClaimSet.
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
-pub struct ClaimSetJS {
+struct ClaimSetJS {
     jti: String,
     aud: String,
     exp: Option<i64>,
@@ -81,14 +120,24 @@ impl ClaimSetJS {
     }
 }
 
+/// The Orizentic Context encapsulates a set of claims and an associated secret. This provides the
+/// overall convenience of easily creating and validating tokens. Generated claimsets are stored
+/// here on the theory that, even with validation, only those claims actually stored in the
+/// database should be considered valid.
 pub struct OrizenticCtx(Secret, HashMap<String, ClaimSet>);
 
+/// An UnverifiedToken is a combination of the JWT serialization and the decoded `ClaimSet`. As this
+/// is unverified, this should only be used for information purposes, such as determining what a
+/// user can do with a token even when the decoding key is absent.
 #[derive(Debug)]
 pub struct UnverifiedToken {
     pub text: String,
     pub claims: ClaimSet,
 }
 
+/// An VerifiedToken is a combination of the JWT serialization and the decoded `ClaimSet`. This will
+/// only be created by the `validate_function`, and thus will represent a token which has been
+/// validated via signature, expiration time, and presence in the database.
 #[derive(Debug)]
 pub struct VerifiedToken {
     pub text: String,
@@ -96,28 +145,14 @@ pub struct VerifiedToken {
 }
 
 impl OrizenticCtx {
+    /// Create a new Orizentic Context with an initial set of claims.
     pub fn new_ctx(secret: Secret, claims_lst: Vec<ClaimSet>) -> OrizenticCtx {
         OrizenticCtx(secret, HashMap::new())
     }
 
-    pub fn decode_text(&self, text: &String) -> Result<UnverifiedToken, Error> {
-        let validation = jwt::Validation{
-            leeway: 0,
-            validate_exp: false,
-            validate_iat: false,
-            validate_nbf: false,
-            aud: None,
-            iss: None,
-            sub: None,
-            ..jwt::Validation::default()
-        };
-        let res = jwt::decode::<ClaimSetJS>(text, &(self.0).0, &validation);
-        match res {
-            Ok(res_) => Ok(UnverifiedToken{text: text.clone(), claims: res_.claims.to_claimset()}),
-            Err(err) => Err(Error::JWTError(err)),
-        }
-    }
-
+    /// Validate a token by checking its signature, that it is not expired, and that it is still
+    /// present in the database. Return an error if any check fails, but return a `VerifiedToken`
+    /// if it all succeeds.
     pub fn validate_token(&self, token: &UnverifiedToken) -> Result<VerifiedToken, Error> {
         let validator = match token.claims.expiration {
             Some(_) => jwt::Validation::default(),
@@ -138,35 +173,48 @@ impl OrizenticCtx {
         }
     }
 
+    /// Given a text string, as from a web application's `Authorization` header, decode the string
+    /// and then validate the token.
     pub fn decode_and_validate_text(&self, text: &String) -> Result<VerifiedToken, Error> {
-        match self.decode_text(text) {
+        // it is necessary to first decode the token because we need the validator to know whether
+        // to attempt to validate the expiration. Without that check, the validator will fail any
+        // expiration set to None.
+        match decode_text(text) {
             Ok(unverified) => self.validate_token(&unverified),
             Err(err) => Err(err),
         }
     }
 
+    /// Add a claimset to the database.
     pub fn add_claimset(&mut self, claimset: ClaimSet) {
         self.1.insert(claimset.id.clone(), claimset);
     }
 
+    /// Remove a claims set from the database so that all additional validation checks fail.
     pub fn revoke_claimset(&mut self, claim: &ClaimSet) {
         self.1.remove(&claim.id);
     }
 
+    /// Revoke a ClaimsSet given its ID, which is set in the `jti` claim of a JWT or the `id` field
+    /// of a `ClaimSet`.
     pub fn revoke_by_uuid(&mut self, claim_id: &String) {
         self.1.remove(claim_id);
     }
 
+    /// *NOT IMPLEMENTED*
     pub fn replace_claimsets(&mut self, claims_lst: Vec<ClaimSet>) { }
 
+    /// List all of the `ClaimSet` IDs in the database.
     pub fn list_claimsets(&self) -> Vec<String> {
         self.1.keys().map(| id | id.clone()).collect()
     }
 
+    /// Find a `ClaimSet` by ID.
     pub fn find_claimset(&self, claims_id: &String) -> Option<&ClaimSet> {
         self.1.get(claims_id)
     }
 
+    /// Encode and sign a claimset, returning the result as a `VerifiedToken`.
     pub fn encode_claimset(&self, claims: &ClaimSet) -> Result<VerifiedToken, Error> {
         let in_db = self.1.get(&claims.id);
         if in_db.is_some() {
@@ -185,10 +233,8 @@ impl OrizenticCtx {
     }
 }
 
-pub fn check_authorizations<F: FnOnce(&ResourceName, &Permissions) -> bool>(f: F, token: &VerifiedToken) -> bool {
-    f(&token.claims.resource, &token.claims.permissions)
-}
-
+/// Create a new `ClaimSet`. This will return a claimset with the expiration time calculated from
+/// the TTL if the TTL is provided. No expiration will be set if no TTL is provided.
 pub fn create_claimset(issuer: Issuer,
                        ttl: Option<TTL>,
                        resource_name: ResourceName,
@@ -209,4 +255,21 @@ pub fn create_claimset(issuer: Issuer,
         permissions: perms,
     }
 }
+
+/// Decode a JWT text string without verification
+pub fn decode_text(text: &String) -> Result<UnverifiedToken, Error> {
+    let res = jwt::dangerous_unsafe_decode::<ClaimSetJS>(text);
+    match res {
+        Ok(res_) => Ok(UnverifiedToken{text: text.clone(), claims: res_.claims.to_claimset()}),
+        Err(err) => Err(Error::JWTError(err)),
+    }
+}
+
+/// Given a `VerifiedToken`, pass the resource name and permissions to a user-defined function. The
+/// function should return true if the caller should be granted access to the resource and false,
+/// otherwise. That result will be passed back to the caller.
+pub fn check_authorizations<F: FnOnce(&ResourceName, &Permissions) -> bool>(f: F, token: &VerifiedToken) -> bool {
+    f(&token.claims.resource, &token.claims.permissions)
+}
+
 
