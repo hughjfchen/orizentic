8dc654cda31a921805565a55ad20ea19be1c9f69
Savanni D'Gerinel <savanni@luminescent-dreams.com>
2019-04-01 09:19:28 -0400

    Install a Rust-1.33 derivation

--- rust/shell.nix
+++ rust/shell.nix
@@ -2,12 +2,16 @@ let
     pkgs = import <nixpkgs-18.09> {};
     unstable = import <nixpkgs> {};
     frameworks = pkgs.darwin.apple_sdk.frameworks;
+    rust = import ./nixpkgs/rust-1.33.nix {
+      mkDerivation = pkgs.stdenv.mkDerivation;
+      fetchurl = pkgs.fetchurl;
+      stdenv = pkgs.stdenv;
+    };
+
 in pkgs.stdenv.mkDerivation {
     name = "orizentic";
 
-    buildInputs = [ unstable.rustc
-                    unstable.cargo
-                    unstable.rustfmt
+    buildInputs = [ rust
                     unstable.carnix
                     frameworks.Security
                     frameworks.CoreFoundation
